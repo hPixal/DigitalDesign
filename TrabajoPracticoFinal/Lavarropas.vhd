ba95f55930baadebbd2755a9251c1a5f20bd921b
hPixal <nahuel.j.gareis@protonmail.com>
2022-06-22 09:07:14 -0300

    tpactualizacion

--- TrabajoPracticoFinal/Lavarropas.vhd
+++ TrabajoPracticoFinal/Lavarropas.vhd
@@ -8,6 +8,7 @@ port(
     led_tapa : out std_logic;
     led_lavado : out std_logic;
     led_centrifugado : out std_logic;
+    clk : in std_logic;
     led_enjuague : out std_logic
 );
 end entity;
@@ -72,6 +73,22 @@ architecture sens of Lavarropas is
     signal sal_sensor3 : std_logic;
     signal sal_sensor4 : std_logic;
 
+    --Botonera
+
+    --signal inicio : std_logic;
+    --signal perilla : std_logic_vector(2 downto 0);
+    --signal led_tapa : std_logic;
+    --signal led_lavado : std_logic;
+    --signal led_centrifugado : std_logic;
+    --signal led_enjuague : std_logic;
+
+    signal state_reg, next_state : std_logic_vector(3 downto 0);
+
+    --Estados
+    Constant IDLE         : std_logic_vector(3 downto 0) := "0001";
+    Constant LAVADO       : std_logic_vector(3 downto 0) := "0010";
+    Constant ENJUAGUE     : std_logic_vector(3 downto 0) := "0100";
+    Constant CENTRIFUGADO : std_logic_vector(3 downto 0) := "1000";
 begin
     --Valvulas
     VS: Valvula port map(
@@ -125,5 +142,90 @@ begin
         sal_sensor => sal_sensor4
     );
 
+    process(next_state)
+    begin
+        if(next_state'event) then
+            state_reg <= next_state;
+        end if;
+    end process;
+
+    process (clk)
+    begin
+      if rising_edge(clk) then
+        delay <= rd_en & delay(0 to dn - 2);
+      end if;
+    end process;
+
+
+    process(perilla,inicio,state_reg)
+    begin
+        case state_reg is
+----------------------------------------------------------------------------------------------------
+            when IDLE=>
+                if inicio = '1' then
+                    if perilla = "001" or perilla = "011" or perilla = "101" or perilla = "111" then
+                        next_state <= LAVADO;
+                    end if;
+                    if perilla = "110" or perilla = "010" then
+                        next_state <= ENJUAGUE;
+                    end if;
+                    if perilla = "100" then
+                        next_state <= CENTRIFUGADO;
+                    end if;
+                else
+                    next_state <= IDLE;
+                end if;
+----------------------------------------------------------------------------------------------------
+            when LAVADO =>
+                act_VJ <= '1';
+                act_VL <= '1';
+                wait for 10 ns;
+                act_VJ <= '0';
+                act_VL <= '0';
+                if sal_sensor3 = '0' then
+                    act_VV <= '1';
+                    med <= '1';
+                    wait for 20 ns;
+                    act_VV <= '0';
+                    med <= '0';
+                    next_state <= IDLE;
+                end if;
+                med <= '1';
+                wait for 600 ns;
+                med <= '0';
+                act_VV <= '1';
+                act_bomba <= '1';
+                wait for 20 ns;
+                act_VV <= '0';
+                act_bomba <= '0';
+                --TODO: Logica de los sensores
+                if perilla = "011" or perilla = "111" then
+                    next_state <= ENJUAGUE;
+                end if;
+                if perilla = "001" then
+                    next_state <= IDLE;
+                end if;
+                if perilla = "101" then
+                    next_state <= CENTRIFUGADO;
+                end if;
+----------------------------------------------------------------------------------------------------
+            when ENJUAGUE =>
+            --TODO: Añadir logica motores
+                if perilla = "011" or perilla = "010" then
+                    next_state <= IDLE;
+                end if;
+                if perilla = "111" or perilla = "110" then
+                    next_state <= CENTRIFUGADO;
+                end if;
+----------------------------------------------------------------------------------------------------
+            when CENTRIFUGADO =>
+                if perilla = "101" or perilla = "111" or perilla = "110" or perilla = "100" then
+                    next_state <= IDLE;
+                end if;
+            
+            when others =>
+            next_state <= IDLE;
+        end case;   
+    end process;
 
 end architecture;
